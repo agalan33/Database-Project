b4258abe661d32b6089fbdd24f6237a40b1d4d47
adahidgalan <adahid.galan@upr.edu>
2019-03-23 09:44:15 -0400

    Added login and create account

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {ChatComponent} from './chat/chat.component'
+import {ChatComponent} from './chat/chat.component';
+import {LoginComponent} from './login/login.component';
+import {CreateAccountComponent} from './create-account/create-account.component';
 
 const routes: Routes = [
-  {path: "chat", component: ChatComponent}
+  {path: "chat", component: ChatComponent},
+  {path: "login", component: LoginComponent},
+  {path: "create_account", component: CreateAccountComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,28 @@ import { AppComponent } from './app.component';
 import { MessageComponent } from './message/message.component';
 import { MaterialComponentsModule } from './material-components/material-components.module';
 import { ChatComponent } from './chat/chat.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component'
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { LoginComponent } from './login/login.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material'
 
 @NgModule({
   declarations: [
     AppComponent,
     MessageComponent,
     ChatComponent,
-    NavBarComponent
+    NavBarComponent,
+    LoginComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialComponentsModule
+    MaterialComponentsModule,
+    MatDatepickerModule,
+    MatNativeDateModule
   ],
   providers: [],
   bootstrap: [AppComponent]
