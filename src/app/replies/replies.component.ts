646b655bd49d18b661e54135068b65edbf0e8894
EduardoPerez13 <eduardo.perez13@upr.edu>
2019-04-07 00:30:05 -0400

    Added reply form

--- src/app/replies/replies.component.ts
+++ src/app/replies/replies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Message } from '../message';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-replies',
@@ -8,10 +9,26 @@ import { Message } from '../message';
 })
 export class RepliesComponent implements OnInit {
   @Input() replies: Message[];
+  replyForm = new FormGroup({
+    mtext: new FormControl('', Validators.required)
+  });
 
   constructor() {}
 
   ngOnInit() {
   }
 
+  onSend(){
+    let newMessage: Message = {
+      mid: 1000,
+      ufirst_name: 'This',
+      ulast_name: 'User',
+      mimage: null,
+      mtext: this.replyForm.value.mtext,
+      likes: 0,
+      dislikes: 0,
+      mdate: '2019-04-07 03:57:23.530286 +00:00'
+    };
+    this.replies.push(newMessage);
+  }
 }
