fdbc66a6f060ba2d07821710a061d7b6fb227a11
EduardoPerez13 <eduardo.perez13@upr.edu>
2019-04-08 01:28:24 -0400

    Added like/dislike toggle-button-group

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -8,10 +8,31 @@ import { Message } from '../message';
 })
 export class MessageComponent implements OnInit {
   @Input() message: Message;
+  likes: number;
+  dislikes: number;
+  selectedValue: string;
+  modified: boolean;
 
   constructor() {}
 
   ngOnInit() {
+    this.modified = false;
+    this.likes = 0;
+    this.dislikes =0;
   }
+
+  selectionChanged(value: any) {
+    if(value == "like"){
+      if(this.modified)
+        this.dislikes--;
+      this.likes++;
+    }else{
+      if(this.modified)
+        this.likes--;
+      this.dislikes++;
+    }
+    this.modified = true;
+  }
+
 
 }
