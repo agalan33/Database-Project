f1705d307a161cf1a8cae0b9879b3451eadd6a84
adahid <adahid.galan@upr.edu>
2019-04-08 22:00:58 -0400

    added all chats

--- src/app/all-chats/all-chats.component.ts
+++ src/app/all-chats/all-chats.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Chats} from '../classes/chats.type';
+import { Chats } from '../classes/chats.type';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-all-chats',
@@ -11,12 +12,18 @@ export class AllChatsComponent implements OnInit {
   public chats = [];
   public chatsURL = 'http://127.0.0.1:5000/DbProject/users/1/chats';
   public headers = ['cname'];
-  constructor(private httpClient: HttpClient) { }
+  public pURL = '/chat';
+  public cURL = '';
+  constructor(private httpClient: HttpClient, private router: Router) { }
 
   ngOnInit() {
     this.httpClient.get<Chats[]>(this.chatsURL).subscribe(data => {
       this.chats = data;
     });
+  }
+
+  goToChat(index: number) {
+    this.router.navigateByUrl(this.pURL);
   }
 
 }
